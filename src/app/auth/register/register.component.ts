72f8a38b5ccf51b2206e85fa9e77cf638290f08b
ivanmartinezv <ivmartez@gmail.com>
2021-02-19 20:29:47 -0300

    2 files modified

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
 import { FormGroup, FormControl } from "@angular/forms";
-
+import { Router } from "@angular/router";
 //import { AuthService } from "@auth/services/auth.service";
 import { AuthService } from "../services/auth.service";
-import { Router } from "@angular/router";
 //import { User } from "@app/shared/models/user.interface";
 import { User } from "../../shared/models/user.interface";
 
@@ -13,17 +12,21 @@ import { User } from "../../shared/models/user.interface";
   styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent {
+  public titulo: string = "formulario de registro";
+
   registerForm = new FormGroup({
     email: new FormControl(""),
     password: new FormControl("")
   });
 
-  constructor(private authSvc: AuthService, private router: Router) {}
+  constructor(private _authService: AuthService, private router: Router) {
+    console.log("register component running");
+  }
 
   async onRegister() {
     const { email, password } = this.registerForm.value;
     try {
-      const user = await this.authSvc.register(email, password);
+      const user = await this._authService.register(email, password);
       if (user) {
         this.checkUserIsVerified(user);
       }
